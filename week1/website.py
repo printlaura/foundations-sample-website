1de70e90d57acf83c538fc5399a71ddd88177b86
Laura Iaies <lauiaies@gmail.com>
2021-03-03 20:21:21 +0100

    add new pages and style

--- week1/website.py
+++ week1/website.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', page_title="CODE")
 
 
 @app.route('/first-page')
 def first_page():
-    return render_template('first-page.html', page_title="First Template")
+    return render_template('first-page.html', page_title="dwight")
 
 
 @app.route('/second-page')
 def second_page():
-    return render_template('second-page.html', page_title="Second Template")
+    return render_template('second-page.html', page_title="contact")
 
 # add additonal pages here using a similar format as above
 
