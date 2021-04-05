a403f7cc4a70da0ceeb4a366fff666a65a152d90
Laura Iaies <lauiaies@gmail.com>
2021-04-05 13:04:43 +0200

    change main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-from week1.website import app  # noqa: F401
-=======
-from covid_app.website import app  # noqa: F401
->>>>>>> upstream/main
+#<<<<<<< HEAD
+from color_check.website import app  # noqa: F401
+#=======
+#from covid_app.website import app  # noqa: F401
+#>>>>>>> upstream/main
