662b74a0e60d889cbc0992b0000c8122683c4682
Laura Iaies <lauiaies@gmail.com>
2021-04-05 14:11:06 +0200

    please work :)

--- color_check/tests/test_colorcheck.py
+++ color_check/tests/test_colorcheck.py
@@ -2,7 +2,7 @@ from color_check.website import app
 from color_check.controllers.get_color_code import get_color_code
 
 
-# test the  function we've written to check on the colors themselves
+# test the  function we've written to check on the colors themselves -
 def test_get_color_code():
     # this test should pass right now
     assert get_color_code("blue") == "#0000ff"
