9e1f5ba623f4f68479797d2843c9f50eca4177f1
Laura Iaies <lauiaies@gmail.com>
2021-04-05 13:09:25 +0200

    add data attribute to post function in test_colors

--- color_check/tests/test_colorcheck.py
+++ color_check/tests/test_colorcheck.py
@@ -33,5 +33,5 @@ def test_index():
 # check that there is a route at "/colors" which accepts a POST request
 def test_colors():
     with app.test_client() as test_client:
-        response = test_client.post('/color')
+        response = test_client.post('/color', data={color:"#0000ff"})
         assert response.status_code == 200
